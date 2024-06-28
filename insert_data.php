8a47dce11271af55ed5c47e5b699d888ecaed003
arpit mishra <arpit.mishra.out@gmail.com>
2024-06-28 11:49:18 +0530

    getting the last id of recent inserted data

--- insert_data.php
+++ insert_data.php
@@ -20,11 +20,16 @@ if($conn->connect_error){
 // if sucessful, then connecting to database and inserting data to the table.
 
 $sql = "INSERT INTO MyGuests(firstname,lastname,email)
-VALUES('Novak','Djokovic','nole.24.out@gmail.com')";
+VALUES('Roger','Federer','roger.federer.in@gmail.com')";
 
 // Checking if query is right and inserting data
 if($conn->query($sql) === true){
-    echo "Data Inserted into table sucessfully!";
+    // Getting the id of the last element updated/inserted to the table.
+    $last_id = $conn->insert_id;
+    echo "Data Inserted into table sucessfully!" . $last_id . "<br>";
 }else{
     echo "Something got wrong!";
 }
+
+// Closing the connection
+$conn->close();
